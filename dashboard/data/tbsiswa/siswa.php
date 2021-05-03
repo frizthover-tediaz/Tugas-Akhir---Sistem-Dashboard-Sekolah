808c12893dec5ea7ae7afad4703501b30e4704ac
Fraz <77263098+frizthover-tediaz@users.noreply.github.com>
2021-05-03 17:49:07 +0700

    perbaiki align data siswa.php
    perbaiki align data siswa.php

--- dashboard/data/tbsiswa/siswa.php
+++ dashboard/data/tbsiswa/siswa.php
@@ -87,7 +87,7 @@ require '../../koneksi.php';
             <div class="card-body">
                 <div class="table-responsive">
                     <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                        <thead>
+                        <thead style="text-align: center">
                             <tr>
                                 <th>Nis</th>
                                 <th>Nama</th>
@@ -97,7 +97,7 @@ require '../../koneksi.php';
                             </tr>
                         </thead>
                         
-                        <tbody>
+                        <tbody style="text-align: center">
                             <?php 
                                 $query = mysqli_query($conn, "SELECT * FROM tbsiswa");
                             ?>
